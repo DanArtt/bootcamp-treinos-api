984c4f83f6a68e3e5b255206e62b2b5feedd5027
DanArtt <xdogman5@gmail.com>
2026-03-03 00:12:18 -0300

    feat: adicionando a tabela do workout session e deletar em cascata no schema

---
+++ src/schemas/index.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+
+import { WeekDay } from "../generated/prisma/enums.js";
+
+export const ErrorSchema = z.object({
+  error: z.string(),
+  code: z.string(),
+});
+
+export const WorkoutPlanSchema = z.object({
+  id: z.uuid(),
+  name: z.string().trim().min(1),
+  workoutDays: z.array(
+    z.object({
+      name: z.string().trim().min(1),
+      weekDay: z.enum(WeekDay),
+      isRest: z.boolean(),
+      estimatedDurationInSeconds: z.number(),
+      exercises: z.array(
+        z.object({
+          order: z.number(),
+          name: z.string(),
+          sets: z.number(),
+          reps: z.number(),
+          restTimeInSeconds: z.number(),
+        }),
+      ),
+    }),
+  ),
+});
